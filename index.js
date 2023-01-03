f20221a9d372db0d8b91b0a59f5101c8f1305418
Tobani <tobanigeorge@gmail.com>
2023-01-03 02:19:06 +0100

    gets all items from catalog listings

--- index.js
+++ index.js
@@ -24,23 +24,28 @@ async function scrapeData(query) {
       $(".pg-w.-ptm.-pbxl").children().length - 1
     ];
   const lastPageUrl = $(lastPageLink).attr("href");
-
   const last_page = extractPageNum(lastPageUrl);
 
   const scrape = async url => {
     // reload new and parse new data
-    const { data } = url ? await axios.get(url.toString()) : undefined;
-    const $ = data ? cheerio.load(data) : $;
+    let data;
+    let _$;
+    if (url) {
+      data = await axios.get(url.toString());
+      _$ = cheerio.load(data.data);
+    } else {
+      _$ = $;
+    }
 
     // break down children and store text-content
-    const list_items = $("article.prd._fb.col.c-prd a");
+    const list_items = _$("article.prd._fb.col.c-prd a");
     list_items.each((i, el) => {
-      let href = $(el).attr("href");
-      let img_container = $(el).children(".img-c");
-      let img_link = $(img_container).children("img").attr("data-src");
-      let info_container = $(el).children(".info");
-      let name = $(info_container).children(".name").text();
-      let price = $(info_container).children(".prc").text();
+      let href = _$(el).attr("href");
+      let img_container = _$(el).children(".img-c");
+      let img_link = _$(img_container).children("img").attr("data-src");
+      let info_container = _$(el).children(".info");
+      let name = _$(info_container).children(".name").text();
+      let price = _$(info_container).children(".prc").text();
       scraped_data.push({
         link: href,
         name,
@@ -53,19 +58,19 @@ async function scrapeData(query) {
 
   for (let i = 1; i <= last_page; i++) {
     if (i === 1) {
-      scrape();
+      await scrape();
     } else {
       url.searchParams.get("page")
         ? url.searchParams.set("page", i)
         : url.searchParams.append("page", i);
-      scrape(url + "#catalog-listing");
+      await scrape(url + "#catalog-listing");
     }
   }
   return scraped_data;
 }
 
 scrapeData(prompt("please provide a search query: ")).then(res =>
-  console.log(/*"content", res,*/ "\nsize", res.length)
+  console.log("content", res, "\nsize", res.length)
 );
 
 module.export = scrapeData;
@@ -74,7 +79,9 @@ module.export = scrapeData;
 // from a pathname that looks like:
 // /catalog/?q=aaa&page=50#catalog-listing
 function extractPageNum(path) {
-  const searchParams = new URLSearchParams(path);
+  const searchParams = new URLSearchParams(
+    path.slice(path.indexOf("page"))
+  );
   const page = searchParams.get("page");
   return parseInt(page);
 }
