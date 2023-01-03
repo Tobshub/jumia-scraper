f847e0e75e5272de2ffce8ead171b10ee8dc32c1
Tobani <tobanigeorge@gmail.com>
2023-01-03 03:53:07 +0100

    added ext function for pagination

--- index.js
+++ index.js
@@ -4,33 +4,35 @@ const pretty = require("pretty");
 const prompt = require("prompt-sync")();
 
 const url = new URL("https://jumia.com.ng/catalog");
+const baseUrl = url.origin; /* jumia origin */
 
 async function scrapeData(query) {
+  // append search query
   url.searchParams.append("q", query);
   const scraped_data = [];
-  // load the html from the url
-  const { data } = await axios.get(url.toString());
 
-  // parse the html with cheerio
+  // load the html from the url and parse with cheerio
+  const { data } = await axios.get(url.toString());
   const $ = cheerio.load(data);
 
-  // set url scrape url to new page pathname if redirected
+  // set scrape url to page pathname if incase of redirect
   const path_name = $("head").children("meta")["5"].attribs.content;
   url.pathname = path_name;
 
-  // get last page to loop over
+  // get last page number
   const lastPageLink =
     $(".pg-w.-ptm.-pbxl").children()[
       $(".pg-w.-ptm.-pbxl").children().length - 1
     ];
   const lastPageUrl = $(lastPageLink).attr("href");
+
   const last_page = extractPageNum(lastPageUrl);
 
   const scrape = async url => {
-    // reload new and parse new data
     let data;
     let _$;
     if (url) {
+      // reload and parse new data
       data = await axios.get(url.toString());
       _$ = cheerio.load(data.data);
     } else {
@@ -47,38 +49,40 @@ async function scrapeData(query) {
       let name = _$(info_container).children(".name").text();
       let price = _$(info_container).children(".prc").text();
       scraped_data.push({
-        link: href,
+        link: baseUrl + href,
         name,
         price,
         thumbnail: img_link,
       });
     });
   };
-  // use a for loop of the max pagination range to get all the items
 
-  for (let i = 1; i <= last_page; i++) {
-    if (i === 1) {
-      await scrape();
-    } else {
-      url.searchParams.get("page")
-        ? url.searchParams.set("page", i)
-        : url.searchParams.append("page", i);
-      await scrape(url + "#catalog-listing");
-    }
-  }
-  return scraped_data;
+  await scrape();
+  // next function returned each time for pagination
+  const next = async (i = 2) => {
+    scraped_data.splice(0, scraped_data.length);
+    url.searchParams.get("page")
+      ? url.searchParams.set("page", i)
+      : url.searchParams.append("page", i);
+    await scrape(url + "#catalog-listing");
+    return {
+      data: scraped_data,
+      next: i < last_page ? () => next(i + 1) : undefined,
+    };
+  };
+  return {
+    data: scraped_data,
+    next: () => next(),
+  };
 }
 
-scrapeData(prompt("please provide a search query: ")).then(res =>
-  console.log("content", res, "\nsize", res.length)
-);
+module.exports = scrapeData;
 
-module.export = scrapeData;
-
-// extracts the last page number
+// extracts the page number
 // from a pathname that looks like:
-// /catalog/?q=aaa&page=50#catalog-listing
+// /catalog/?page=50#catalog-listing
 function extractPageNum(path) {
+  if (!path) return 1;
   const searchParams = new URLSearchParams(
     path.slice(path.indexOf("page"))
   );
